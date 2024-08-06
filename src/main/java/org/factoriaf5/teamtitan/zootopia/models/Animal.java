efa4970275c6aff4bc715928236a232f54589f65
Danil22May <daniilmej88@gmail.com>
2024-08-06 12:52:55 +0200

    Animal model added

---
+++ src/main/java/org/factoriaf5/teamtitan/zootopia/models/Animal.java
@@ -0,0 +1,83 @@
+package org.factoriaf5.teamtitan.zootopia.models;
+
+import java.sql.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Animals {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String type;
+    private String family;
+    private String genre;
+    private Date date;
+    private String image;
+
+    public Animals() {
+    }
+
+    public Animals(String name, String type, String family, String genre, Date date, String image) {
+        this.name = name;
+        this.type = type;
+        this.family = family;
+        this.genre = genre;
+        this.date = date;
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getFamily() {
+        return family;
+    }
+
+    public void setFamily(String family) {
+        this.family = family;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
